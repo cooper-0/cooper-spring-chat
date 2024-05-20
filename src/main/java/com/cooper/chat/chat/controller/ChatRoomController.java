9774bab28ed5a3f15febd241b30bdc5c3b1eac5a
um0772 <ops0044@naver.com>
2024-05-21 05:21:21 +0900

    ChatRoomController 추가

--- src/main/java/com/cooper/chat/chat/controller/ChatRoomController.java
+++ src/main/java/com/cooper/chat/chat/controller/ChatRoomController.java
@@ -4,6 +4,8 @@ import com.cooper.chat.chat.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.cooper.chat.chat.model.ChatRoom;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/room")
@@ -21,5 +23,17 @@ public class ChatRoomController {
         // 채팅방 이름으로 컬렉션 생성
         chatService.createChatCollection(roomId);
         return ResponseEntity.ok("Chat room created successfully");
+    }
+    // 채팅방 삭제 엔드포인트
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteRoom(@RequestParam String roomId) {
+        chatService.deleteRoom(roomId);
+        return ResponseEntity.ok("채팅방이 삭제되었습니다.");
+    }
+
+    // 채팅방 목록 불러오기 엔드포인트
+    @GetMapping("/list")
+    public List<String> getChatRooms() {
+        return chatService.getCollectionNames();
     }
 }
